b89aaa74946ce88fb621f03e79af5cdf4b0ac033
Devon Adair <devonadair96@gmail.com>
2017-05-09 01:01:38 -0400

    added rouge class

---
+++ DnDCharacterApp/src/souce/Rouge.java
@@ -0,0 +1,12 @@
+package souce;
+
+public class Rouge extends Character {
+	
+//	private ArrayList<>
+
+	public Rouge(String name, int amount) {
+		super(name, amount);
+		// TODO Auto-generated constructor stub
+	}
+
+}
